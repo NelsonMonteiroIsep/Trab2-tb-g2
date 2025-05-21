36ac5a33362b398111ac400cc8856ba4555a73ab
Barbosa-code <1241626@isep.ipp.pt>
2025-05-21 21:01:04 +0100

    Pagina Market (topbar)

--- Crescendo/src/main/java/isep/crescendo/controller/MarketController.java
+++ Crescendo/src/main/java/isep/crescendo/controller/MarketController.java
@@ -1,14 +1,18 @@
 package isep.crescendo.controller;
 
+
 import isep.crescendo.model.User;
 import isep.crescendo.util.SceneSwitcher;
 import isep.crescendo.util.SessionManager;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.stage.Stage;
+import javafx.scene.image.ImageView;
+
 
 import java.io.IOException;
 import java.net.URL;
@@ -17,7 +21,8 @@ import java.util.ResourceBundle;
 public class MarketController implements Initializable {
     @FXML
     private Label userNameLabel;
-
+    @FXML
+    private ImageView goToOtherPage;
     private User loggedInUser;
 
     @Override
@@ -25,14 +30,25 @@ public class MarketController implements Initializable {
         loggedInUser = SessionManager.getCurrentUser();
 
         if (loggedInUser != null) {
-            userNameLabel.setText("Bem-vindo, " + loggedInUser.getNome());
+            userNameLabel.setText("Crescendo.");
         } else {
-            userNameLabel.setText("Bem-vindo, visitante!");
+            userNameLabel.setText("Crescendo.");
         }
     }
     @FXML
     private void handleLogout() {
         SessionManager.setCurrentUser(null);
         SceneSwitcher.switchScene("/isep/crescendo/login-view.fxml", "/isep/crescendo/styles/login.css", "Login", userNameLabel);
+    }
+    @FXML
+    private void handleImageClick() {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource(""));
+            Parent root = loader.load();
+            Stage stage = (Stage) goToOtherPage.getScene().getWindow(); // Pega a janela atual
+            stage.setScene(new Scene(root));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
