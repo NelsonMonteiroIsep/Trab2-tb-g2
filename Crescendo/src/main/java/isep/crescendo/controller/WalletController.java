55bb64f20bcd814b237f6398fdd044f0b060ead0
Barbosa-code <1241626@isep.ipp.pt>
2025-06-04 21:40:56 +0100

    Carteira FE feita
    So falta listar cripto e colocar o dinheiro disponivel e o investido

--- Crescendo/src/main/java/isep/crescendo/controller/WalletController.java
+++ Crescendo/src/main/java/isep/crescendo/controller/WalletController.java
@@ -1,8 +1,28 @@
 package isep.crescendo.controller;
 
+import isep.crescendo.model.User;
+import isep.crescendo.util.SessionManager;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
 import java.awt.event.ActionEvent;
+
 
 public class WalletController {
 
+    @FXML
+    private Label userNameLabel;
 
+    private User loggedInUser;
+
+    @FXML
+    private void initialize() {
+        loggedInUser = SessionManager.getCurrentUser();
+
+        if (loggedInUser != null) {
+            userNameLabel.setText("Bem-vindo, " + loggedInUser.getNome());
+        } else {
+            userNameLabel.setText("Bem-vindo, visitante!");
+        }
+    }
 }
